09344814fb9dd8831e8808264d3f1c9eb137ad81
alexdikelsky <alexander.dikelsky@gmail.com>
2021-11-04 16:04:31 -0500

    Fix bug

--- script.js
+++ script.js
@@ -22,3 +22,24 @@ async function run_num() {
     console.log(v)
 }
 
+async function get_id_jokes(id, lang, cat) {
+    let url = `http://flask-env.eba-kizmemsb.us-east-2.elasticbeanstalk.com/api/v1/jokes?language=${lang}&id=${id}&category=${cat}`
+    console.log(url)
+    let elements = await fetch(url)
+        .then(response => response.json())
+        .catch(error => console.log(error));
+
+    return elements
+}
+
+async function run_id() {
+    let num  = document.getElementById("idn").value
+    let cat  = document.getElementById("category").value
+    let lang = document.getElementById("language").value
+
+    let base = document.getElementById("jokes")
+
+    let v = get_num_jokes(num, lang, cat);
+    console.log(v)
+}
+
